36fb208c4d0d30356ac580d3fda06e1a11c1dd70
jinhee02 <jinhee02@g.shingu.ac.kr>
2023-09-19 10:57:41 +0900

    유니티 1주차 수업 커밋 (2021.3.19)

---
+++ My project/Assets/Scrpits/AlembicPlayController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Formats.Alembic.Importer;
+
+public class AlembicPlayController : MonoBehaviour
+{
+    public AlembicStreamPlayer streamPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        QualitySettings.vSyncCount = 0;
+        streamPlayer = GetComponent<AlembicStreamPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        streamPlayer.CurrentTime += Time.deltaTime;
+    }
+}
