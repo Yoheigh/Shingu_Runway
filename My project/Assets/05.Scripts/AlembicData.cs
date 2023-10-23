c7c907fe0186b24cb606165ee4745b10d8dacef3
이상신 <dygp25@naver.com>
2023-10-24 04:31:16 +0900

    feat: "AlembicDatabaseSO 생성"

--- My project/Assets/05.Scripts/AlembicData.cs
+++ My project/Assets/05.Scripts/AlembicData.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 using UnityEngine.Formats.Alembic.Importer;
 using static Define;
 
+[System.Serializable]
 public class AlembicData
 {
-    public string id;
+    public int id;
     public string name;
     public ClothesType type;
     public string description;
     public Sprite image;
+    public GameObject prefab;
 }
